3bb7a3ab1318f08df33ad71985fc079f9f4c1e68
Stuart <146047128+strtgbb@users.noreply.github.com>
2024-12-20 16:21:34 -0500

    use kwargs for hclient init

--- testflows/github/hetzner/runners/hclient.py
+++ testflows/github/hetzner/runners/hclient.py
@@ -5,12 +5,10 @@ from . import __version__ as project_version, __name__ as project_name
 
 class HClient(Client):
 
-    def __init__(
-        self,
-        token,
-        api_endpoint="https://api.hetzner.cloud/v1",
-        poll_interval=1,
-    ):
+    def __init__(self, *args, **kwargs):
         super().__init__(
-            token, api_endpoint, project_name, project_version, poll_interval
+            *args,
+            application_name=project_name,
+            application_version=project_version,
+            **kwargs,
         )
